eee01c4a50c07a43cdb60f125c4a594e52563431
Margaret Alade <alade.margaret@yandex.ru>
2024-05-09 19:43:56 +0300

    Initial commit

--- Task-1.1 Thread/main.cpp
+++ Task-1.1 Thread/main.cpp
@@ -6,9 +6,36 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+int client_count = 0;
+
+void client(int max_num) {
+    for (int i = 0; i < max_num; i++) {
+        std::this_thread::sleep_for(1000ms);
+        client_count += 1;
+        std::cout << "Клиент зашел в очередь: client_count = " << client_count << "\n";
+    }
+}
+
+void operat(int max_num) {
+    for (int i = 0; i < max_num; i++) {
+        std::this_thread::sleep_for(2000ms);
+        client_count -= 1;
+        std::cout << "Запрос клинта обработан: client_count = " << client_count << "\n";
+    }
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::thread th1(client, 10);
+    std::thread th2(operat, 10);
+    th1.join();
+    th2.join();
+    std::cout << "Итоговое состояние счетчика клиентов: " << client_count << "\n";
+    
+    
     return 0;
 }
